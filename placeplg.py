5bf0de1a7196c6ee865283571ac0e486b86af754
ZiverW <ziverw@163.com>
2015-03-28 23:12:02 +0800

    small changes

--- placeplg.py
+++ placeplg.py
@@ -12,10 +12,8 @@ exh = open('workdir\\exheader.bin','rb')
 exh.seek(512)
 lid = str(hex(struct.unpack('I',exh.read(4))[0]))[2:]
 hid = str(hex(struct.unpack('I',exh.read(4))[0]))[2:]
-if len(lid)<8:
-                lid = '0'*(8-len(lid))+lid
-if len(hid)<8:
-                hid = '0'*(8-len(hid))+hid
+lid = '0'*(8-len(lid))+lid
+hid = '0'*(8-len(hid))+hid
 jumpid = hid+lid
 mkdir('workdir\\plugin\\'+jumpid)
 plg = open('workdir\\layeredfs.plg','rb')
